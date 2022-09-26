3eafbc3853079d84311e516e085b90671ce54ac7
Budakov Dmitry <budakov.it@gmail.com>
2022-09-27 01:45:38 +0300

    Update routes

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import NewWords from 'src/pages/NewWords'
 import Interactive from 'src/pages/Interactive'
 import Results from 'src/pages/Results'
@@ -8,13 +8,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Outlet />}>
-          <Route index element={<Home />} />
-          <Route path="new-words" element={<NewWords />} />
-          <Route path="interactive" element={<Interactive />} />
-          <Route path="results" element={<Results />} />
-          <Route path="*" element={<h2>Page Not Found</h2>} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/new-words" element={<NewWords />} />
+        <Route path="/interactive" element={<Interactive />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="*" element={<h2>Page Not Found</h2>} />
       </Routes>
     </BrowserRouter>
   )
